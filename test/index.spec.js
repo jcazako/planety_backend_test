62b6fda80883852a534b13edbfa04ae6afddb8e6
jcazako <jeancharles.cazako@gmail.com>
2021-12-05 20:23:16 +0100

    refactored sanitizer

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
 import { users, infos, jobs, result } from './api.js'
-import Sanitizer from '../lib/index.js'
+import sanitizer from '../lib/index.js'
 
 describe('backend test', () => {
   it('should concat and sanitize correctly the data', () => {
-    const output = new Sanitizer(users, infos, jobs).run()
+    const output = sanitizer(users, infos, jobs)
     expect(output).toEqual(result)
   })
 })
