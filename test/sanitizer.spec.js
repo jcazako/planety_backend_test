74253b96c4363517647ad8798a2aa6a54bce0a85
jcazako <jeancharles.cazako@gmail.com>
2021-12-05 20:47:30 +0100

    isolated sanitizer

--- test/sanitizer.spec.js
+++ test/sanitizer.spec.js
@@ -1,7 +1,7 @@
 import { users, infos, jobs, result } from './api.js'
-import sanitizer from '../lib/index.js'
+import { sanitizer } from '../lib/sanitizer.js'
 
-describe('backend test', () => {
+describe('sanitizer test', () => {
   it('should concat and sanitize correctly the data', () => {
     const output = sanitizer(users, infos, jobs)
     expect(output).toEqual(result)
